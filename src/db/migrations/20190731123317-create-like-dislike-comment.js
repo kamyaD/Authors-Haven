4a63fc7e376f88a39cf56a1a3410cf759c19b9b1
manzif <48624152+manzif@users.noreply.github.com>
2019-08-02 09:01:29 +0200

    feat(like-comment): user likes a specific comment (#42)
    - create a database for liking a comment
     - create like a comment
     - create dislike a comment
     - return a total number of likes at every like and dislike
    
    [Finishes #166789893]

---
+++ src/db/migrations/20190731123317-create-like-dislike-comment.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('LikeDislikeComments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      commentId: {
+        type: Sequelize.INTEGER
+      },
+      like: {
+        type: Sequelize.BOOLEAN
+      },
+      dislike: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('LikeDislikeComments');
+  }
+};
---
+++ tests/likeDislikeComment.js
@@ -0,0 +1,89 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import index from '../src/index';
+
+
+chai.use(chaiHttp);
+chai.should();
+chai.expect();
+dotenv.config();
+
+const userToken = jwt.sign({
+  id: 3,
+  username: 'testUser',
+  email: 'user@gmail.com',
+  role: 'admin'
+}, process.env.SECRET_JWT_KEY, { expiresIn: '24h' });
+
+const userToken1 = jwt.sign({
+  id: 1,
+  username: 'testUser',
+  email: 'test@gmail.com',
+  role: 'admin'
+}, process.env.SECRET_JWT_KEY, { expiresIn: '24h' });
+
+const like = {
+  like: true,
+  dislike: false,
+  userId: 3,
+  commentId: 2
+};
+const dislike = {
+  like: false,
+  dislike: true,
+  userId: 1,
+  commentId: 2
+};
+describe('like Comment test', () => {
+  before('should create a new comment', (done) => {
+    const comment = { body: 'this like' };
+    chai.request(index)
+      .post('/api/comments/articles/TIA')
+      .set('token', userToken)
+      .send(comment)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.eql(201);
+        res.body.should.have.property('message');
+      });
+    done();
+  });
+  it('should like a  comment', (done) => {
+    chai.request(index)
+      .post('/api/comments/like/comment/2')
+      .set('token', userToken)
+      .send(like)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.eql(201);
+        res.body.should.have.property('message');
+      });
+    done();
+  });
+  it('should not like a  comment when there is no comment', (done) => {
+    chai.request(index)
+      .post('/api/comments/like/comment/0')
+      .set('token', userToken)
+      .send(like)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.eql(404);
+        res.body.should.have.property('error');
+      });
+    done();
+  });
+  it('should dislike a  comment', (done) => {
+    chai.request(index)
+      .post('/api/comments/dislike/comment/3')
+      .set('token', userToken1)
+      .send(dislike)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.status.should.be.eql(201);
+        res.body.should.have.property('message');
+      });
+    done();
+  });
+});
