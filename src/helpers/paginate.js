04396fca54d1c83ef94737da713ab6cc0eabab37
Manzi Fabrice <manzif60@gmail.com>
2019-09-07 15:32:37 +0200

    bg(share-article): Users should be able to share articles on social media and via email
     - add open package to help share artcle on different social media
    
    [Finishes]

--- src/helpers/paginate.js
+++ src/helpers/paginate.js
@@ -3,9 +3,7 @@ const paginate = (page, pageSize) => {
   if (!page) {
     page = 1;
   }
-  if (!pageSize) {
-    pageSize = 10;
-  }
+  pageSize = 30;
   const offset = (page - 1) * (pageSize);
   const limit = pageSize;
 
